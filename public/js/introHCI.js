5b9eb4f6570d4bac215c7f09f8c659dc7b8bbd1f
Tyler Li <Tyler Li>
2017-02-16 13:43:12 -0800

    AJAX

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,29 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+		
+		//$("div #project" + idNumber  + " .details").html(projectHTML);
+
+}
+
+function addProject(result) {
+
+	var resultID = result['id'];
+
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p>' + '<p>' + result['summary'] + '</p>';
+
+    $("#project" + resultID + " .details").html(projectHTML);
+    //$("#project-container").html(projectHTML);
+    //$("#project-description").html(result['summary']);
+
+    console.log(result);
+
 }
 
 /*
@@ -35,4 +57,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", changeColor);
 }
+
+function changeColor(result){
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
